deeb668f7abb6d6f3ea51f4aee14f3848a6d0b77
Yuan Wang <yn416226@gmail.com>
2023-06-12 17:11:28 -0300

    use mysql dump files to generate sql modifying tables

--- mysql_merge_tool/dump_comparison.py
+++ mysql_merge_tool/dump_comparison.py
@@ -1,4 +1,5 @@
 import re
+import utils
 
 
 def generate_create_tables_queries(source_dump, target_dump):
@@ -43,9 +44,23 @@ def _parse_dump(dumpsql):
     return database
 
 
-# TODO
 def generate_modify_tables_queries(source_dump, target_dump):
-    pass
+    comment = "-- Modify structures of updated tables:\n"
+    queries = []
+    source_db = _parse_dump(source_dump)
+    target_db = _parse_dump(target_dump)
+
+    # intersection set: tables that both databases have
+    inter_tables = set(source_db.keys()) & set(target_db.keys())
+
+    for table in inter_tables:
+        source_table_def = utils.parse_table_definition(source_db.get(table))
+        target_table_def = utils.parse_table_definition(target_db.get(table))
+        table_updates = utils.get_table_updates(source_table_def, target_table_def)
+        query = utils.generate_modify_table_sql(table, table_updates)
+        queries.append(query)
+
+    return comment + "".join(queries) + "\n"
 
 
 def generate_merge_sql(source_dump, target_dump):
